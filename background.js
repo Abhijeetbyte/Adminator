df5db466135c2e64bbca399d40ea7cec05e6cee0
Abhijeet Kumar <80936610+Abhijeetbyte@users.noreply.github.com>
2023-06-12 11:09:40 +0530

    Update background.js for V3

--- background.js
+++ background.js
@@ -1,8 +1,26 @@
-// Listen for tab updates
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    if (changeInfo.status === "complete" && tab.active && tab.url.includes("youtube.com/watch")) {
-      // Send a message to the content script
-      chrome.tabs.sendMessage(tabId, { action: "videoStarted" });
-    }
-  });
-  
+// background.js
+
+// Log a message when the extension is installed or updated
+chrome.runtime.onInstalled.addListener(details => {
+    //console.log('Extension installed or updated:', details.reason);
+});
+
+// Add a listener for the extension icon click event
+chrome.action.onClicked.addListener(tab => {
+    //console.log('Extension icon clicked');
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: simulateKeyCombination,
+    });
+});
+
+// Simulate the key combination
+function simulateKeyCombination() {
+    //console.log('Simulating key combination');
+    document.dispatchEvent(new KeyboardEvent('keydown', { key: 'Alt' }));
+    document.dispatchEvent(new KeyboardEvent('keydown', { key: 'ArrowLeft' }));
+    document.dispatchEvent(new KeyboardEvent('keyup', { key: 'ArrowLeft' }));
+    document.dispatchEvent(new KeyboardEvent('keydown', { key: 'ArrowRight' }));
+    document.dispatchEvent(new KeyboardEvent('keyup', { key: 'ArrowRight' }));
+    document.dispatchEvent(new KeyboardEvent('keyup', { key: 'Alt' }));
+}
